c0ebde3e2caf01c416013db2d7fa01d8ee8f62f0
dashabull <dasha.bull@gmail.com>
2022-09-13 09:31:48 -0400

    created services

--- src/main/java/DAOclasses/AirlineDAO.java
+++ src/main/java/DAOclasses/AirlineDAO.java
@@ -1,4 +1,25 @@
 package DAOclasses;
+import flightInfo.Airline;
+import utils.ConnectionPool;
+
+import java.sql.Connection;
 
 public class AirlineDAO {
+    public Airline getAirlineByID(int id) {
+        Connection c = ConnectionPool.getInstance().getConnection();
+        ConnectionPool.getInstance().returnConnection(c);
+        return null;
+    }
+    public void createAirline(Airline a) {
+        Connection c = ConnectionPool.getInstance().getConnection();
+        ConnectionPool.getInstance().returnConnection(c);
+    }
+    public void updateAirline(Airline a) {
+        Connection c = ConnectionPool.getInstance().getConnection();
+        ConnectionPool.getInstance().returnConnection(c);
+    }
+    public void deleteAirline(Airline a) {
+        Connection c = ConnectionPool.getInstance().getConnection();
+        ConnectionPool.getInstance().returnConnection(c);
+    }
 }
